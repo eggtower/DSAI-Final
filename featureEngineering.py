40dd7c6576bb28e8219f41e8f592f2044ffac9b1
johnson870930@gmail.com <johnson870930@gmail.com>
2022-06-07 15:06:40 +0800

    feat: feature engineering

---
+++ featureEngineering.py
@@ -0,0 +1,277 @@
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 100)
+
+from itertools import product
+from sklearn.preprocessing import LabelEncoder
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import time
+import sys
+import gc
+import pickle
+
+def plot_features(booster, figsize):    
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    return plot_importance(booster=booster, ax=ax)
+
+items = pd.read_csv('../input/items.csv')
+shops = pd.read_csv('../input/shops.csv')
+cats = pd.read_csv('../input/item_categories.csv')
+train = pd.read_csv('../input/sales_train.csv')
+# set index to ID to avoid droping it later
+test  = pd.read_csv('../input/test.csv').set_index('ID')
+
+# 移除離群值
+train = train[train.item_price<50000]
+train = train[train.item_cnt_day<750]
+# 將 < 0 的數值以中位數補值
+median = train[(train.shop_id==32)&(train.item_id==2973)&(train.date_block_num==4)&(train.item_price>0)].item_price.median()
+train.loc[train.item_price<0, 'item_price'] = median
+
+# 將相同店名的商店設為同類
+train['shop_id']=train['shop_id'].replace({0:57,1:58,11:10,40:39,23:24})
+test['shop_id']=test['shop_id'].replace({0:57,1:58,11:10,40:39,23:24}) 
+
+# 對 Shops/Cats/Items 資料前處理 獨立出 type code
+shops.loc[shops.shop_name == 'Сергиев Посад ТЦ "7Я"', 'shop_name'] = 'СергиевПосад ТЦ "7Я"'
+shops['city'] = shops['shop_name'].str.split(' ').map(lambda x: x[0])
+shops.loc[shops.city == '!Якутск', 'city'] = 'Якутск'
+shops['city_code'] = LabelEncoder().fit_transform(shops['city'])
+shops = shops[['shop_id','city_code']]
+cats['split'] = cats['item_category_name'].str.split('-')
+cats['type'] = cats['split'].map(lambda x: x[0].strip())
+cats['type_code'] = LabelEncoder().fit_transform(cats['type'])
+# if subtype is nan then type
+cats['subtype'] = cats['split'].map(lambda x: x[1].strip() if len(x) > 1 else x[0].strip())
+cats['subtype_code'] = LabelEncoder().fit_transform(cats['subtype'])
+cats = cats[['item_category_id','type_code', 'subtype_code']]
+
+items.drop(['item_name'], axis=1, inplace=True)
+
+# 月銷售
+ts = time.time()
+matrix = []
+cols = ['date_block_num','shop_id','item_id']
+for i in range(34):
+    sales = train[train.date_block_num==i]
+    matrix.append(np.array(list(product([i], sales.shop_id.unique(), sales.item_id.unique())), dtype='int16'))
+    
+matrix = pd.DataFrame(np.vstack(matrix), columns=cols)
+matrix['date_block_num'] = matrix['date_block_num'].astype(np.int8)
+matrix['shop_id'] = matrix['shop_id'].astype(np.int8)
+matrix['item_id'] = matrix['item_id'].astype(np.int16)
+matrix.sort_values(cols,inplace=True)
+time.time() - ts
+
+train['revenue'] = train['item_price'] *  train['item_cnt_day']
+
+ts = time.time()
+group = train.groupby(['date_block_num','shop_id','item_id']).agg({'item_cnt_day': ['sum']})
+group.columns = ['item_cnt_month']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=cols, how='left')
+matrix['item_cnt_month'] = (matrix['item_cnt_month']
+                                .fillna(0)
+                                .clip(0,20) # NB clip target here
+                                .astype(np.float16))
+time.time() - ts
+
+# Test set
+test['date_block_num'] = 34
+test['date_block_num'] = test['date_block_num'].astype(np.int8)
+test['shop_id'] = test['shop_id'].astype(np.int8)
+test['item_id'] = test['item_id'].astype(np.int16)
+
+# Merge features
+ts = time.time()
+matrix = pd.merge(matrix, shops, on=['shop_id'], how='left')
+matrix = pd.merge(matrix, items, on=['item_id'], how='left')
+matrix = pd.merge(matrix, cats, on=['item_category_id'], how='left')
+matrix['city_code'] = matrix['city_code'].astype(np.int8)
+matrix['item_category_id'] = matrix['item_category_id'].astype(np.int8)
+matrix['type_code'] = matrix['type_code'].astype(np.int8)
+matrix['subtype_code'] = matrix['subtype_code'].astype(np.int8)
+time.time() - ts
+
+# 滯後
+def lag_feature(df, lags, col):
+    tmp = df[['date_block_num','shop_id','item_id',col]]
+    for i in lags:
+        shifted = tmp.copy()
+        shifted.columns = ['date_block_num','shop_id','item_id', col+'_lag_'+str(i)]
+        shifted['date_block_num'] += i
+        df = pd.merge(df, shifted, on=['date_block_num','shop_id','item_id'], how='left')
+    return df
+# 滯後 1,2,3,6,12 個月
+ts = time.time()
+matrix = lag_feature(matrix, [1,2,3,6,12], 'item_cnt_month')
+time.time() - ts
+
+# 取 Mean feature
+def meanEncoder(matrix, groupList, columnName, lags):
+    ts = time.time()
+    group = matrix.groupby(groupList).agg({'item_cnt_month': ['mean']})
+    group.columns = [ columnName ]
+    group.reset_index(inplace=True)
+
+    matrix = pd.merge(matrix, group, on=groupList, how='left')
+    matrix[columnName] = matrix[columnName].astype(np.float16)
+    matrix = lag_feature(matrix, lags, columnName)
+    matrix.drop([columnName], axis=1, inplace=True)
+    time.time() - ts
+    return matrix
+# Mean features
+matrix = meanEncoder(matrix, ['date_block_num'], 'date_avg_item_cnt', [1])
+matrix = meanEncoder(matrix, ['date_block_num', 'item_id'], 'date_item_avg_item_cnt', [1,2,3,6,12])
+matrix = meanEncoder(matrix, ['date_block_num', 'shop_id'], 'date_shop_avg_item_cnt',[1,2,3,6,12])
+matrix = meanEncoder(matrix, ['date_block_num', 'item_category_id'], 'date_cat_avg_item_cnt', [1])
+matrix = meanEncoder(matrix, ['date_block_num', 'shop_id', 'item_category_id'], 'date_shop_cat_avg_item_cnt',[1])
+matrix = meanEncoder(matrix, ['date_block_num', 'shop_id', 'type_code'], 'date_shop_type_avg_item_cnt', [1])
+matrix = meanEncoder(matrix, ['date_block_num', 'shop_id', 'subtype_code'], 'date_shop_subtype_avg_item_cnt', [1])
+matrix = meanEncoder(matrix, ['date_block_num', 'city_code'], 'date_city_avg_item_cnt', [1])
+matrix = meanEncoder(matrix, ['date_block_num', 'item_id', 'city_code'], 'date_item_city_avg_item_cnt', [1])
+matrix = meanEncoder(matrix, ['date_block_num', 'type_code'], 'date_type_avg_item_cnt', [1])
+matrix = meanEncoder(matrix, ['date_block_num', 'subtype_code'], 'date_subtype_avg_item_cnt', [1])
+
+# Trend features
+# Price trend for the last six months.
+ts = time.time()
+group = train.groupby(['item_id']).agg({'item_price': ['mean']})
+group.columns = ['item_avg_item_price']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['item_id'], how='left')
+matrix['item_avg_item_price'] = matrix['item_avg_item_price'].astype(np.float16)
+
+group = train.groupby(['date_block_num','item_id']).agg({'item_price': ['mean']})
+group.columns = ['date_item_avg_item_price']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num','item_id'], how='left')
+matrix['date_item_avg_item_price'] = matrix['date_item_avg_item_price'].astype(np.float16)
+
+lags = [1,2,3,4,5,6]
+matrix = lag_feature(matrix, lags, 'date_item_avg_item_price')
+
+for i in lags:
+    matrix['delta_price_lag_'+str(i)] = \
+        (matrix['date_item_avg_item_price_lag_'+str(i)] - matrix['item_avg_item_price']) / matrix['item_avg_item_price']
+
+def select_trend(row):
+    for i in lags:
+        if row['delta_price_lag_'+str(i)]:
+            return row['delta_price_lag_'+str(i)]
+    return 0
+    
+matrix['delta_price_lag'] = matrix.apply(select_trend, axis=1)
+matrix['delta_price_lag'] = matrix['delta_price_lag'].astype(np.float16)
+matrix['delta_price_lag'].fillna(0, inplace=True)
+
+fetures_to_drop = ['item_avg_item_price', 'date_item_avg_item_price']
+for i in lags:
+    fetures_to_drop += ['date_item_avg_item_price_lag_'+str(i)]
+    fetures_to_drop += ['delta_price_lag_'+str(i)]
+
+matrix.drop(fetures_to_drop, axis=1, inplace=True)
+
+time.time() - ts
+
+# Last month shop revenue trend
+ts = time.time()
+group = train.groupby(['date_block_num','shop_id']).agg({'revenue': ['sum']})
+group.columns = ['date_shop_revenue']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['date_block_num','shop_id'], how='left')
+matrix['date_shop_revenue'] = matrix['date_shop_revenue'].astype(np.float32)
+
+group = group.groupby(['shop_id']).agg({'date_shop_revenue': ['mean']})
+group.columns = ['shop_avg_revenue']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['shop_id'], how='left')
+matrix['shop_avg_revenue'] = matrix['shop_avg_revenue'].astype(np.float32)
+
+matrix['delta_revenue'] = (matrix['date_shop_revenue'] - matrix['shop_avg_revenue']) / matrix['shop_avg_revenue']
+matrix['delta_revenue'] = matrix['delta_revenue'].astype(np.float16)
+
+matrix = lag_feature(matrix, [1], 'delta_revenue')
+
+matrix.drop(['date_shop_revenue','shop_avg_revenue','delta_revenue'], axis=1, inplace=True)
+time.time() - ts
+
+# Month
+matrix['month'] = matrix['date_block_num'] % 12
+# Date
+days = pd.Series([31,28,31,30,31,30,31,31,30,31,30,31])
+matrix['days'] = matrix['month'].map(days).astype(np.int8)
+# 商店最後一次銷售紀錄
+ts = time.time()
+cache = {}
+matrix['item_shop_last_sale'] = -1
+matrix['item_shop_last_sale'] = matrix['item_shop_last_sale'].astype(np.int8)
+for idx, row in matrix.iterrows():    
+    key = str(row.item_id)+' '+str(row.shop_id)
+    if key not in cache:
+        if row.item_cnt_month!=0:
+            cache[key] = row.date_block_num
+    else:
+        last_date_block_num = cache[key]
+        matrix.at[idx, 'item_shop_last_sale'] = row.date_block_num - last_date_block_num
+        cache[key] = row.date_block_num         
+time.time() - ts
+# 最後一次銷售紀錄
+ts = time.time()
+cache = {}
+matrix['item_last_sale'] = -1
+matrix['item_last_sale'] = matrix['item_last_sale'].astype(np.int8)
+for idx, row in matrix.iterrows():    
+    key = row.item_id
+    if key not in cache:
+        if row.item_cnt_month!=0:
+            cache[key] = row.date_block_num
+    else:
+        last_date_block_num = cache[key]
+        if row.date_block_num>last_date_block_num:
+            matrix.at[idx, 'item_last_sale'] = row.date_block_num - last_date_block_num
+            cache[key] = row.date_block_num         
+time.time() - ts
+# 每個商店/物品對和物品的首次銷售後的月數
+ts = time.time()
+matrix['item_shop_first_sale'] = matrix['date_block_num'] - matrix.groupby(['item_id','shop_id'])['date_block_num'].transform('min')
+matrix['item_first_sale'] = matrix['date_block_num'] - matrix.groupby('item_id')['date_block_num'].transform('min')
+time.time() - ts
+
+# 刪除不含滯後的 dataset
+ts = time.time()
+matrix = matrix[matrix.date_block_num > 11]
+time.time() - ts
+
+# 移除 none 欄位
+ts = time.time()
+def fill_na(df):
+    for col in df.columns:
+        if ('_lag_' in col) & (df[col].isnull().any()):
+            if ('item_cnt' in col):
+                df[col].fillna(0, inplace=True)         
+    return df
+
+matrix = fill_na(matrix)
+time.time() - ts
+
+matrix.to_pickle('data.pkl')
+del matrix
+del cache
+del group
+del items
+del shops
+del cats
+del train
+# leave test for submission
+gc.collect()
